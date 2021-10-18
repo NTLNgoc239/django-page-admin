0c9a8f4d3da13b65ad8c52a2554bb99643a98e4e
NTLNgoc <nguyenngochcmus@gmail.com>
2021-10-18 15:25:39 +0700

    delete row in asgi

--- home/asgi.py
+++ home/asgi.py
@@ -14,5 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home.settings')
 
 application = get_asgi_application()
-
-//thêm chỗ này vài dòng nè, check xem có thấy không
