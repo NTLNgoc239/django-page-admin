ec247099ed4cd726a02daafd2cc72fd79e8108ea
NTLNgoc <nguyenngochcmus@gmail.com>
2021-10-20 10:39:34 +0700

    edit sidebar and search

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -4,14 +4,18 @@
 // 			.sidebar('toggle')
 // 	});
 // });
-function search() {
+
+function Search() {
+  // Declare variables
   var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
+  input = document.getElementById("searchArticles");
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[2];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -22,6 +26,7 @@ function search() {
     }
   }
 }
+
 function onToggle() {
   var rowId = event.target.parentNode.parentNode.id;
   // console.log(rowId);
